331f4845ec6d3326b3e4832724181aa496a89f67
kofoworola <kofoworola@KOFOWOROLA-PC>
2017-01-20 17:13:56 +0100

    Appointments as events

--- modules/admin/controllers/calendar_beta.php
+++ modules/admin/controllers/calendar_beta.php
@@ -1,10 +1,24 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
 
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
 
-class calendar_beta extends MX_Controller{
-    
-    public function _construct()
-    {
+class calendar_beta extends MX_Controller {
+
+    public function __construct() {
         parent::__construct();
+        $this->auth->is_logged_in();
+        $this->load->model("appointment_model");
+        $this->load->model("to_do_list_model");
     }
+
+
+    function index() {
+        $admin = $this->session->userdata('admin');
+        $data['appointments'] = $this->appointment_model->get_appointment_by_doctor($admin['id']);
+        $data['page_title'] = lang('event_calendar');
+        $data['body'] = 'calendar/beta';
+        $this->load->view('template/main', $data);
+    }
+
 }
--- modules/admin/views/calendar/beta.php
+++ modules/admin/views/calendar/beta.php
@@ -1,8 +1,58 @@
+<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<link href="<?php echo base_url('assets/css/jquery.datetimepicker.css') ?>" rel="stylesheet" type="text/css" />
+<link rel='stylesheet' href='<?php echo base_url('assets/fullcalendar/fullcalendar.css'); ?>' />
+<script src='<?php echo base_url('assets/fullcalendar/lib/jquery.min.js') ?>'></script>
+<script src='<?php echo base_url('assets/fullcalendar/lib/moment.min.js') ?>'></script>
+<script src='<?php echo base_url('assets/fullcalendar/fullcalendar.js') ?>'></script>
+<script src="<?php echo base_url('assets/js/chosen.jquery.min.js') ?>" type="text/javascript"></script>
+
+<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<script src="<?php echo base_url('assets/js/') ?>/custom.js" type="text/javascript"></script>   
+
+<script type="text/javascript">
+    $(document).ready(function () {
+
+        $('#calendar').fullCalendar({
+        header: {
+        left: 'prev,next today',
+                center: 'title',
+                right: 'month,agendaWeek,agendaDay,listWeek'
+        },
+                events: [
 <?php
+if (isset($appointments)) {
+    foreach ($appointments as $appointment) {
+        ?>
+                        {
+                        id:'<?php echo $appointment->id ?>',
+                                title:'Appointment "<?php echo $appointment->title ?>',
+                                start:'<?php echo $appointment->date ?>'
+                        },
+    <?php }
+}
+?>
+                ]
+        });
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    });
+</script>
 
+<section class="content-header">
+    <h1>
+<?php echo $page_title; ?>
+
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="<?php echo site_url('admin') ?>"><i class="fa fa-dashboard"></i> <?php echo lang('dashboard') ?></a></li>
+        <li class="active"><?php echo lang('event_calendar') ?></li>
+    </ol>
+</section>
+<section class="content">
+    <div class="row">
+        <div class="box-primary">
+            <div style="padding: 10px 10px;">
+                <div id="calendar"></div>
+            </div>
+        </div>
+    </div>
+</section>
